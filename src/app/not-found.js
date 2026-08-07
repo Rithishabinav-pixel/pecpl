78cfd92f799cb2abc796773cb6dad0ea76d36438
Rithish Abinav <rithish.pixel@gmail.com>
2026-08-07 15:08:51 +0530

    Add

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './not-found.module.css'
 import Button from './components/ui/button'
+import Image from 'next/image'
 
 export default function NotFound() {
   return (
@@ -9,9 +10,9 @@ export default function NotFound() {
     <section>
       <div className="container">
         <div className={style.number}>
-            <h1>404</h1>
-            <h2>ERROR 🙁</h2>
-            <h3>SORRY.. PAGE NOT FOUND!</h3>
+            <Image className={style.image} src="/assets/images/404.svg" width={2364} height={1194} alt='404'/>
+            <h2 className={`heading ${style.heading}`}>ERROR 🙁</h2>
+            <h3 className={`heading ${style.heading}`}>SORRY.. PAGE NOT FOUND!</h3>
             <p>The page are you looking for doesn’t exist…</p>
             <Button classname="light_blue" href="/" text="Return home"/>
         </div>
