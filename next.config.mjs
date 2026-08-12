bc6cc125337db34f4f3968757f7df9f209367a63
Rithish Abinav <rithish.pixel@gmail.com>
2026-08-12 15:30:38 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-   trailingSlash: true, // or false
+  trailingSlash: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/products/equipment-spares/",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/cryogenic-heat-exchangers/",
+        destination: "/products/shell-and-tube-heat-exchangers#cryogenic",
+        permanent: true,
+      },
+      {
+        source: "/products/helical-baffle-heat-exchangers/",
+        destination: "/products/shell-and-tube-heat-exchangers#helixchanger",
+        permanent: true,
+      },
+      {
+        source: "/products/rod-baffle-exchangers/",
+        destination: "/products/shell-and-tube-heat-exchangers#rod",
+        permanent: true,
+      },
+      {
+        source: "/products/process-packages/",
+        destination: "/products/pressure-vessels",
+        permanent: true,
+      },
+      {
+        source: "/job-openings/",
+        destination: "/career",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
