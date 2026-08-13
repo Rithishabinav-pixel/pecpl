a81ce1ddc5790af05827bc4f8aaa4c73d40b73b2
Rithish Abinav <rithish.pixel@gmail.com>
2026-08-13 18:16:23 +0530

    Add

--- next.config.mjs
+++ next.config.mjs
@@ -35,6 +35,51 @@ const nextConfig = {
         destination: "/career",
         permanent: true,
       },
+      {
+        source: "/pdf/NB_R_STAMP_CERTIFICATE.pdf",
+        destination: "/assets/pdf/cert8.pdf",
+        permanent: true,
+      },
+       {
+        source: "/pdf/NB_R_STAMP_CERTIFICATE.pdf",
+        destination: "/assets/pdf/cert4.pdf",
+        permanent: true,
+      },
+        {
+        source: "/pdf/iso-certificate.pdf",
+        destination: "/assets/pdf/cert1.pdf",
+        permanent: true,
+      },
+      {
+        source: "/pdf/ASME-U2.pdf",
+        destination: "/assets/pdf/cert4.pdf",
+        permanent: true,
+      },
+      {
+        source: "/pdf/pecpl-u-stamp.pdf",
+        destination: "/assets/pdf/cert4.pdf",
+        permanent: true,
+      },
+      {
+        source: "/wp-content/uploads/2025/06/rod_baffle.pdf",
+        destination: "/assets/pdf/newsletter/rod_baffle.pdf",
+        permanent: true,
+      },
+       {
+        source: "/wp-content/uploads/2025/07/certificates.pdf",
+        destination: "/assets/pdf/cert4.pdf",
+        permanent: true,
+      },
+      {
+        source: "/pdf/ISO-CERTIFICATE-VALID%20U-TO-31ST-DECEMBER-2023.pdf",
+        destination: "/assets/pdf/cert4.pdf",
+        permanent: true,
+      },
+      {
+        source: "/wp-content/uploads/2025/07/STH-Zertifikat-29a-ISO-3834-EN-signed-SvR.pdf",
+        destination: "/assets/pdf/cert4.pdf",
+        permanent: true,
+      },
     ];
   },
 };
